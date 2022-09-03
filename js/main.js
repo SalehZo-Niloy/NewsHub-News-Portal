cc5dd539cb40dc5633a604d49dc3c80ee96896ec
SalehZo-Niloy <msnniloy7@gmail.com>
2022-09-03 14:17:07 +0600

    news cards section added

--- js/main.js
+++ js/main.js
@@ -17,15 +17,43 @@ const displayCategories = categories => {
         categoryBtn.addEventListener('click', async () => {
             const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category.category_id}`);
             const data = await res.json();
-            console.log(data);
+            console.log(data.data);
+            displayNews(data.data);
         }
         )
     });
 }
 
 // display news divs
-const displayNews = () => {
-    const newsPost = document.getElementById('news-preview');
+const displayNews = (allNews) => {
+    const newsPostSection = document.getElementById('news-preview');
+    newsPostSection.innerHTML = '';
+    const newsCountSection = document.getElementById
+    allNews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML = `
+        <div
+            class="flex flex-col items-center bg-gray-200 rounded-lg shadow-md md:flex-row w-3/4 hover:bg-gray-300
+            my-4 mx-auto">
+            <img class="object-cover w-full h-96 rounded-t-lg md:h-56 md:w-72 md:rounded-none md:rounded-l-lg"
+                src="${news.image_url}" alt="">
+            <div class="w-full flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">${news.title}</h5>
+                <p class="mb-3 font-normal text-gray-700">${news.details.slice(0, 250)}...</p>
+                <div class="flex flex-row justify-around items-center">
+                    <div class="flex items-center">
+                        <img class="h-9 rounded-full" src="${news.author.img ? news.author.img : '#'}">
+                        <h3 class="text-sm font-medium mx-1">${news.author.name ? news.author.name : "Author Not Found"}</h3>
+                    </div>
+                    <div><i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view : 'No Views'}</div>
+                    <div><button class="bg-blue-700 text-white rounded px-3 py-1">Details</button></div>
+                </div>
+            </div>
+        </div>
+        `
+        newsPostSection.appendChild(newsDiv);
+    })
+
 
 }
 
